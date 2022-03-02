a0c8dea359525ead0387c72cc94132fcfa62effa
Aditia <aditiafa112@gmail.com>
2022-03-02 22:06:33 +0700

    Implement api Get Banner pada Setting Banner

--- src/pages/SettingBanner/index.js
+++ src/pages/SettingBanner/index.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
 import {FAB, Header, SettingCard} from '../../components';
-
-const imgDummy = [
-  {
-    id: 1,
-    img: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
-    title: 'Nama Banner',
-  },
-  {
-    id: 2,
-    img: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80',
-    title: 'Nama Banner',
-  },
-];
+import {apiGetListBanner} from '../../api/Banner';
 
 const SettingBanner = () => {
+  const [banner, setBanner] = React.useState([]);
+
+  const getInitialValue = async () => {
+    const newBanner = await apiGetListBanner();
+    setBanner(newBanner);
+  };
+
+  React.useEffect(() => {
+    getInitialValue();
+  },[])
+
   return (
     <View style={styles.page}>
       <Header title={'Setting Banner'} />
       <ScrollView style={styles.scrollView}>
-        {Array.isArray(imgDummy) &&
-          imgDummy.map((item, index) => {
+        {Array.isArray(banner) &&
+          banner.map((item, index) => {
             return (
               <View key={index} style={styles.separator}>
-                <SettingCard id={item.id} img={item.img} title={item.title} />
+                <SettingCard id={item.id} img={item.photo} title={item.title} />
               </View>
             );
           })}
