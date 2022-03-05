0c10c5654644ba337d4cc42cf2edf211c2c53866
Aditia <aditiafa112@gmail.com>
2022-03-05 12:46:46 +0700

    pushnotification

--- App.js
+++ App.js
@@ -2,8 +2,18 @@ import React from 'react';
 import Router from './src/router';
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
+import messaging from '@react-native-firebase/messaging';
+import {Alert} from 'react-native';
 
 const App = () => {
+  React.useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert(remoteMessage?.notification?.title, remoteMessage?.notification?.body);
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <>
       <Provider store={store}>
